b80166c3ee0c17edb37188dca55025f704fdb7b1
llevieux <llevieux@users.noreply.github.com>
2015-10-20 08:50:47 -0700

    cleaning up (I didn't write this code)

--- post-data.php
+++ post-data.php
@@ -8,28 +8,16 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Post Data Template-Part File
- *
- * @file           post-data.php
- * @package        Responsive
- * @author         Emil Uzelac
- * @copyright      2003 - 2014 CyberChimps
- * @license        license.txt
- * @version        Release: 1.1.0
- * @filesource     wp-content/themes/responsive/post-data.php
- * @link           http://codex.wordpress.org/Templates
- * @since          available since Release 1.0
- */
-?>
-
-<?php if ( !is_page() && !is_search() ) { ?>
+if ( !is_page() && !is_search() ) { ?>
 
 	<div class="post-data">
-		<?php printf( __( 'Posted in %s', 'responsive' ), get_the_category_list( ', ' ) ); ?>
-	  <?php //the_tags( __( 'Tagged with:', 'responsive' ) . ' ', ', ', '<br />' ); //commented out to remove functionality?>
+	<?php 
+	printf( __( 'Posted in %s', 'responsive' ), get_the_category_list( ', ' ) );
+	//the_tags( __( 'Tagged with:', 'responsive' ) . ' ', ', ', '<br />' ); //commented out to remove functionality
+	?>
 	</div><!-- end of .post-data -->
 
-<?php } ?>
+<?php } //end if
+?>
 
 <div class="post-edit"><?php edit_post_link( __( 'Edit', 'responsive' ) ); ?></div>
