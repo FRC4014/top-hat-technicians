36d32ebe7227531edbf793b0dc4f593ea2603106
llevieux <llevieux@lucaslevieux.com>
2016-01-21 18:31:12 -0800

    added custom 404 error page

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+// Exit if accessed directly
+if (!defined( 'ABSPATH' ))
+	exit;
+/**
+ * The page for displaying 404 pages (Not Found)
+ */
+?>
+<?php get_header(); ?>
+
+<div id="content-full" class="grid col-940">
+
+	<?php responsive_entry_before(); ?>
+	<div id="post-0" class="error404">
+		<?php responsive_entry_top(); ?>
+
+		<div class="post-entry">
+
+			
+		</div><!-- end of .post-entry -->
+
+		<?php responsive_entry_bottom(); ?>
+	</div><!-- end of #post-0 -->
+	<?php responsive_entry_after(); ?>
+
+</div><!-- end of #content-full -->
+
+<?php get_footer(); ?>
